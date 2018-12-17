44b4687bc783a5d712c80a5f5c81071547418ee5
Thomas Gwynne-Timothy <thomas.gt@live.com>
2018-12-17 09:34:02 -0500

    Update join script and README.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # session-scripts
 A collection of Python scripts for working with Sedeen session files.
+
+## Author Join
+
+This script joins session files created by different authors into a single session file. The options are below:
+```
+usage: Join sedeen session files produced by multiple authors
+       [-h] [-v] [-o OUTPUT_DIR] [-c] dir
+
+positional arguments:
+  dir                   path to source directory containing author subfolders
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         increase output verbosity
+  -o OUTPUT_DIR, --output-dir OUTPUT_DIR
+                        directory to store output session file
+  -c, --color-by-author
+                        override color by author
+```
+
+Here's an example usage:
+```
+python scripts\author_join.py path\to\data -v -c
+```
+
+In this example, `path\to\data` contains subfolders with the names of the authors. Each subfolder would contain a session file. It would look something like this:
+```
+path\to\data
+├── Dr. House\
+│   └── sample.session.xml
+├── Dr. Wilson\
+│   └── sample.session.xml
+└── Dr. Taub\
+    └── sample.session.xml
+```
+
+The `-v` option turns on verbose output so you can debug any problems. The `-c` option strips the existing colors in each session file and adds new colors to help distinguish annotations by author.
+
+
--- scripts/author_join.py
+++ scripts/author_join.py
@@ -1,22 +1,29 @@
+#!/usr/bin/env python3
+
 # author_join.py
 # Join session files created by different authors into a single file.
 # Set the source directory to a folder that contains folders for each author.
 # The name of the author (surrounded by square brackets) will be appended to
 # the description fields of their overlays.
 # Example directory structure:
-# some_directory/
-# ├── Dr. House/
-# │   └── sample.session.xml
-# ├── Dr. Wilson/
-# │   └── sample.session.xml
-# └── Dr. Taub/
-#     └── sample.session.xml
+
 
 import argparse
 import copy
 import os
 import re
 import xml.etree.ElementTree as ET
+
+T10_COLORS = ["#4e79a7ff",
+              "#59a14fff",
+              "#9c755fff",
+              "#f28e2bff",
+              "#edc948ff",
+              "#bab0acff",
+              "#e15759ff",
+              "#b07aa1ff",
+              "#76b7b2ff",
+              "#ff9da7ff"]
 
 # define command line arguments
 parser = argparse.ArgumentParser(
@@ -28,55 +35,74 @@ parser.add_argument(
     "dir", help="path to source directory containing author subfolders")
 parser.add_argument("-o", "--output-dir",
                     help="directory to store output session file", default=".")
+parser.add_argument("-c", "--color-by-author",
+                    help="override color by author", action="store_true")
 
 # parse command line arguments
 args = parser.parse_args()
 
+# if the provided dir is absolute, use it; otherwise, make it absolute
+if os.path.isabs(args.dir):
+    source_dir = args.dir
+else:
+    source_dir = os.path.join(os.getcwd(), args.dir)
+
 if args.verbose:
-    print(F"looking for subfolders in {args.dir}")
+    print(F"looking for subfolders in {source_dir}")
 
 # get the author folders in the specified directory
 authors = [name for name in os.listdir(
-    args.dir) if os.path.isdir(name)]
+    source_dir) if os.path.isdir(os.path.join(source_dir, name))]
 
 if len(authors) < 2:
-    print("error - {} must contain at least 2 subdirectories (contains {})".format(args.dir, len(authors)))
+    print("error - {} must contain at least 2 subdirectories (contains {})".format(source_dir, len(authors)))
     raise SystemExit
 
 if args.verbose:
     for author in authors:
         print(F"  found author {author}")
+    print("looking for common files to merge")
 
 # get full paths to the author folders
-author_dirs = [os.path.join(args.dir, author) for author in authors]
+author_dirs = [os.path.join(source_dir, author) for author in authors]
 
-# ensure each author folder contain similar session files
-filenames = [f for f in os.listdir(author_dirs[0]) if os.path.isfile(f)]
+# ensure each author folder contains similar session files
+filenames = set([f for f in os.listdir(author_dirs[0]) if os.path.isfile(os.path.join(author_dirs[0], f))])
+
 for dir in author_dirs[1:]:
-    pass
+    filenames_tmp = set([f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))])
+    filenames.intersection_update(filenames_tmp)
 
-# filename=os.listdir(authors[0])[0]
-# filepath=os.path.join(args.output_dir, filename)
+if args.verbose:
+    for f in filenames:
+        print(F"  found file {f}")
 
-# if args.verbose:
-#     print(F"joining files into {filepath}")
+for filename in filenames:
+    if args.verbose:
+        print(F"joining file {filename}")
 
-# # Use the first file as base; clear all its overlays
-# tree = ET.parse(authors[0] + '/' + filename)
-# root = tree.getroot()
-# image = root.find('image')
-# overlay = image.find('overlays')
-# for graphic in overlay.findall('graphic'):
-#     overlay.remove(graphic)
+    # use the first file as a reference; clear all its overlays
+    tree_out = ET.parse(os.path.join(author_dirs[0], filename))
+    root_out = tree_out.getroot()
+    image_out = root_out.find('image')
+    overlay_out = image_out.find('overlays')
+    for graphic in overlay_out.findall('graphic'):
+        overlay_out.remove(graphic)
 
-# # Get each authors session and append it to the base
-# for author in authors:
-#     t = ET.parse(author + '/' + filename)
-#     r = t.getroot()
-#     i = r.find('image')
-#     o = i.find('overlays')
-#     for graphic in o.findall('graphic'):
-#         overlay.append(graphic)
-#         graphic.attrib['description'] += ' [' + author + ']'
+    # Get each authors session and append it to the base
+    for i, author in enumerate(authors):
+        tree = ET.parse(os.path.join(author_dirs[i], filename))
+        root = tree.getroot()
+        image = root.find('image')
+        overlays = image.find('overlays')
+        for graphic in overlays.findall('graphic'):
+            overlay_out.append(graphic)
+            graphic.attrib['description'] += ' [' + author + ']'
+            if args.color_by_author:
+                pen = graphic.find('pen')
+                pen.attrib['color'] = T10_COLORS[i]
 
-# tree.write(filename)
+
+    # save the joined session file
+    filepath_out = os.path.join(args.output_dir, filename)
+    tree_out.write(filepath_out)
